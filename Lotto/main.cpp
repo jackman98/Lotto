970790646ba803a6fb5612b215f1c8b115c093b3
skorobogatskiidima <skorobogatskiidima@gmail.com>
2018-01-11 01:38:57 +0200

    Fix main.cpp

--- Lotto/main.cpp
+++ Lotto/main.cpp
@@ -4,6 +4,7 @@
 #include "carddistribution.h"
 #include "card.h"
 #include <QDebug>
+#include <sack.h>
 
 int main(int argc, char *argv[])
 {
@@ -17,36 +18,33 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<Card>("GameClasses", 1, 0, "Card");
     qmlRegisterType<Player>("GameClasses", 1, 0, "Player");
+    qmlRegisterType<Sack>("GameClasses", 1, 0, "Sack");
     qmlRegisterType<CardDistribution>("GameClasses", 1, 0, "CardDistribution");
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
-    qRegisterMetaType<QVector<Card*> >("QVector<Card*> >");
-    //qRegisterMetaType<Player *>("Player *");
-    //qRegisterMetaType<Card *>("Card *");
-
-    Player p1, p2;
-    CardDistribution dis;
-    dis.appointCardsToPlayer(&p1);
-    dis.appointCardsToPlayer(&p2);
+//    Player p1, p2;
+//    CardDistribution dis;
+//    dis.appointCardsToPlayer(&p1);
+//    dis.appointCardsToPlayer(&p2);
 
 
 
-    qDebug() << "PLAYER1 CARDS: ";
-    for(Card *card : p1.getCards()) {
-        qDebug() << card->getAllNumbers();
-        CardGeneration::showCard(card);
-        qDebug() << "\n";
-    }
+//    qDebug() << "PLAYER1 CARDS: ";
+//    for(Card *card : p1.getCards()) {
+//        qDebug() << card->getAllNumbers();
+//        CardGeneration::showCard(card);
+//        qDebug() << "\n";
+//    }
 
-    qDebug() << "PLAYER2 CARDS: ";
-    for(Card *card : p2.getCards()) {
-        qDebug() << card->getAllNumbers();
-        CardGeneration::showCard(card);
-        qDebug() << "\n";
-    }
+//    qDebug() << "PLAYER2 CARDS: ";
+//    for(Card *card : p2.getCards()) {
+//        qDebug() << card->getAllNumbers();
+//        CardGeneration::showCard(card);
+//        qDebug() << "\n";
+//    }
 
     return app.exec();
 }
